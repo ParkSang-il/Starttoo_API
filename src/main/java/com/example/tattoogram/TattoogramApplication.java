cb1b1b4555e4f0c3676497676bb09616960dfea8
ParkSI <teamgod2@naver.com>
2023-01-17 15:56:31 +0900

    commit

---
+++ src/main/java/com/example/tattoogram/TattoogramApplication.java
@@ -0,0 +1,13 @@
+package com.example.tattoogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TattoogramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TattoogramApplication.class, args);
+    }
+
+}
