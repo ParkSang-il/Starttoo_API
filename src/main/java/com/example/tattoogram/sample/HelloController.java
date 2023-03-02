59895c5ce176480acfc2f3dace8f67dde6fba190
park_si <pju2251@nate.com>
2023-03-02 20:25:06 +0900

    webhooks test

--- src/main/java/com/example/tattoogram/sample/HelloController.java
+++ src/main/java/com/example/tattoogram/sample/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/")
     public String index() {
-        return "Hello, Spring Boot!";
+        return "Hello, Spring Boot!test";
     }
 
     @RequestMapping("/test")
