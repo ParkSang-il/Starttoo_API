f8c59a064e2f23035290bb07f0ad5d91f26a5ec2
박상일 <pju2251@nate.com>
2023-08-20 19:21:27 +0900

    API JWT AUTH

--- src/test/java/me/silvernine/tutorial/JwtTutorialApplicationTests.java
+++ src/test/java/me/silvernine/tutorial/JwtTutorialApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.tattoogram;
+package me.silvernine.tutorial;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TattoogramApplicationTests {
+class JwtTutorialApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
